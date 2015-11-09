bab617d4ce37361e897aa0b042d7b16c88359b38
Juniper Chapman <junipie@gmail.com>
2015-11-09 11:37:31 -0700

    Server and package update in preparation for passport implementation, several new files

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ var mongoose = require('mongoose');
 var app = express();
 var uriUtil = require('mongodb-uri');
 
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+// var configDB = require('./config/database.js');
+
 
 var options = {
   server:  { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
@@ -68,6 +78,24 @@ app.get('/', function(req, res) {
 
 app.use('/html', express.static('html'));
 app.use('/static', express.static('static'));
+
+//Passport
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+// require('./app/routes.js')(app, passport);
+
 
 
 // Start the webserver
