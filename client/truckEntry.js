a75b0c87e547e6cbb203cd54c666a904837e4c8e
zfalen <zachfalen@Zachs-MacBook-Pro-3.local>
2015-11-09 15:23:32 -0700

    Truck Entry now supports choosing location with a Google Pin

--- client/truckEntry.js
+++ client/truckEntry.js
@@ -2,6 +2,45 @@ var React = require('react');
 import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
 
 var TruckEntryForm = React.createClass({
+    
+        getInitialState: function(){
+            return{tempLat: 46.869693, tempLng: -113.997044};
+        },
+
+        onMapCreated(map) {
+            
+            var self = this;
+            
+            map.setOptions({
+              disableDefaultUI: true
+            });
+            
+            google.maps.event.addDomListener(window, "resize", function() {
+                    google.maps.event.trigger(map, "resize");
+                    map.setCenter({lat: self.state.tempLat, lng: self.state.tempLng}); 
+            });
+            
+            google.maps.event.addListenerOnce(map, "click", function(event) {
+                    var locationMarker = new google.maps.Marker ({
+                                            position: event.latLng,
+                                            map: map,
+                                            draggable: true
+                                        });
+                
+                    self.setState({tempLat: event.latLng.lat(), tempLng: event.latLng.lng()});
+                    map.setCenter({lat: self.state.tempLat, lng: self.state.tempLng}); 
+                
+                    console.log(self.state.tempLat + " " + self.state.tempLng);
+                
+                    google.maps.event.addListener(locationMarker, 'dragend', function(evt){
+                            self.setState({tempLat: evt.latLng.lat(), tempLng: evt.latLng.lng()});
+                            console.log(self.state.tempLat + " " + self.state.tempLng);
+                            map.setCenter({lat: self.state.tempLat, lng: self.state.tempLng}); 
+                    });
+            });
+        
+          },
+    
         render: function() {
             return (
                 <div>
@@ -9,64 +48,30 @@ var TruckEntryForm = React.createClass({
                       <form action="/api/trucks" method="POST" role="form">
                         <h3>Truck Entry Form</h3>
                         <div className="form-group">
-
-                          <div className="container-fluid">
-                            <div className="col-md-6">
+                    <div className="container-fluid">    
+                        <div className="col-md-6">
+                            <div className="container-fluid">
                               <label>Truck Name:</label>
                               <input type="text" className="form-control" name="truckName" placeholder="Truck Name"/>
-                            </div>
-
-                            <div className="col-md-6">
-                              <label>City:</label>
-                              <input type="text" className="form-control" name="city" placeholder="City and State of operation:"/>
-                            </div>
-                          </div><br/>
-
-                          <div className="container-fluid">
-                            <div className="col-md-6">
+                                <br/>
+                            
                               <label>Description:</label>
                               <input type="text" className="form-control" name="description" placeholder="Tell us about the truck!"/>
-                            </div>
-
-                           <div className="col-md-6">
-                              <label>Picture:</label>
-                              <input type="text" className="form-control" name="image" ref="image" placeholder="Copy an Instagram image link here:"/>
-                            </div>
-                          </div><br/>
-
-                          <div className="container-fluid">
-                            <div className="col-md-6">
+                                <br/>
+                            
                               <label>Cuisine:</label>
                               <input type="text" className="form-control" name="cuisine[]" placeholder="Food type #1"/>
                               <input type="text" className="form-control" name="cuisine[]" placeholder="Food type #2"/>
-                            </div>
-
-                            <div className="col-md-6">
-                              <label>Location:</label>
-                                <div className="container-fluid">
-                                <div className="col-md-6">
-                                    <label>Lat:</label>
-                                    <input type="text" className="form-control" name="lat" placeholder="Copy your lat here:"/>
-                                  </div>
-
-                                  <div className="col-md-6">
-                                    <label>Lon:</label>
-                                    <input type="text" className="form-control" name="lon" placeholder="Copy your lon here:"/>
-                                  </div>
-                                </div>
-                            </div>
-                          </div><br/>
-
-                          <div className="container-fluid">
-                            <div className="col-md-6">
-
+                                <br/>
+                            
                               <label>Hours:</label>
+                                
                               <div className="row">
                                 <div className="col-md-2">
                                   <h5>Monday</h5>
                                 </div>
                                 <div className="col-md-5">
-                                  <input type="text" className="form-control" name="monTime" value="Closed"/>
+                                  <input type="text" className="form-control" name="monTime" defaultValue="Closed"/>
                                 </div>
                               </div>
 
@@ -75,7 +80,7 @@ var TruckEntryForm = React.createClass({
                                   <h5>Tuesday</h5>
                                 </div>
                                 <div className="col-md-5">
-                                  <input type="text" className="form-control" name="tuesTime" value="Closed"/>
+                                  <input type="text" className="form-control" name="tuesTime" defaultValue="Closed"/>
                                 </div>
                               </div>
 
@@ -84,7 +89,7 @@ var TruckEntryForm = React.createClass({
                                   <h5>Wednesday</h5>
                                 </div>
                                 <div className="col-md-5">
-                                  <input type="text" className="form-control" name="wedTime" value="Closed"/>
+                                  <input type="text" className="form-control" name="wedTime" defaultValue="Closed"/>
                                 </div>
                               </div>
 
@@ -93,7 +98,7 @@ var TruckEntryForm = React.createClass({
                                   <h5>Thursday</h5>
                                 </div>
                                 <div className="col-md-5">
-                                  <input type="text" className="form-control" name="thurTime" value="Closed"/>
+                                  <input type="text" className="form-control" name="thurTime" defaultValue="Closed"/>
                                 </div>
                               </div>
 
@@ -102,7 +107,7 @@ var TruckEntryForm = React.createClass({
                                   <h5>Friday</h5>
                                 </div>
                                 <div className="col-md-5">
-                                  <input type="text" className="form-control" name="friTime" value="Closed"/>
+                                  <input type="text" className="form-control" name="friTime" defaultValue="Closed"/>
                                 </div>
                               </div>
 
@@ -111,7 +116,7 @@ var TruckEntryForm = React.createClass({
                                   <h5>Saturday</h5>
                                 </div>
                                 <div className="col-md-5">
-                                  <input type="text" className="form-control" name="satTime" value="Closed"/>
+                                  <input type="text" className="form-control" name="satTime" defaultValue="Closed"/>
                                 </div>
                               </div>
 
@@ -120,36 +125,72 @@ var TruckEntryForm = React.createClass({
                                   <h5>Sunday</h5>
                                 </div>
                                 <div className="col-md-5">
-                                  <input type="text" className="form-control" name="sunTime" value="Closed"/>
+                                  <input type="text" className="form-control" name="sunTime" defaultValue="Closed"/>
                                 </div>
                               </div>
-
                             </div>
-                            <div className="col-md-6">
-                              <div>
+                        </div> 
+                          
+                        <div className="col-md-6">
+                          <div className="container-fluid">
+                          <label>City:</label>
+                          <input type="text" className="form-control" name="city" placeholder="City and State of operation:"/>
+                              <br/>
+                            
+                          <label>Picture:</label>
+                          <input type="text" className="form-control" name="image" ref="image" placeholder="Copy an Instagram image link here:"/>
+                              <br/>
+                            
+                            <div>
+                            <label>Location:</label>
+                            <Gmaps
+                                width={"100%"}
+                                height={'50vh'}
+                                lat={46.869693}
+                                lng={-113.997044}
+                                zoom={16}
+                                loadingMessage={'Be happy'}
+                                params={{v: '3.exp'}}
+                                onMapCreated={this.onMapCreated}>
+
+                            </Gmaps>
+                            <input className="col-md-6 latLngInput" name="lat" value={this.state.tempLat.toString()}/>
+                            <input className="col-md-6 latLngInput" name="lon" value={this.state.tempLng.toString()}/>
+                            </div>
+                            
+                            
+                        </div>
+                       </div>
+                    </div>
+                            <br/>
+                            <br/>
+
+                          <div className="container-fluid">
+                              <div className="col-md-4">
                                 <label>Category:</label><br/>
                                 <input type="checkbox" name="timeCategory[]" ref="timeCategory" value="lunch"/>Lunch<br/>
                                 <input type="checkbox" name="timeCategory[]" ref="timeCategory" value="dinner"/>Dinner<br/>
                                 <input type="checkbox" name="timeCategory[]" ref="timeCategory" value="other"/>Other<br/>
-                              </div><br/>
-                              <div>
+                              </div>
+                              <div className="col-md-4">
                                 <label>Payment Options:</label><br/>
                                 <input type="checkbox" name="payment[]" ref="payment" value="fa fa-money"/>Cash<br/>
                                 <input type="checkbox" name="payment[]" ref="payment" value="fa fa-credit-card"/>Card<br/>
                                 <input type="checkbox" name="payment[]" ref="payment" value="./img/checkbook.png"/>Checks<br/>
-                              </div><br/><br/><br/>
-                            </div>
-                        </div>
+                              </div>
+                              <div className="col-md-4">
+                                  <label>Eating preference options:</label><br/>
+                                  <input type="checkbox" name="foodOptions[]" ref="foodOptions" value="./img/vegan.png"/>Vegan<br/>
+                                  <input type="checkbox" name="foodOptions[]" ref="foodOptions" value="./img/organic.png"/>Organic<br/>
+                                  <input type="checkbox" name="foodOptions[]" ref="foodOptions" value="./img/glutenFree.png"/>Gluten Free<br/>
+                              </div>
+                        </div><br/>
 
 
 
                             <div className="container-fluid">
                               <div className="row">
                                 <div className="col-md-6">
-                                  <label>Eating preference options:</label><br/>
-                                  <input type="checkbox" name="foodOptions[]" ref="foodOptions" value="./img/vegan.png"/>Vegan<br/>
-                                  <input type="checkbox" name="foodOptions[]" ref="foodOptions" value="./img/organic.png"/>Organic<br/>
-                                  <input type="checkbox" name="foodOptions[]" ref="foodOptions" value="./img/glutenFree.png"/>Gluten Free<br/>
                                 </div><br/>
 
                                 <div className="container-fluid">
@@ -165,8 +206,10 @@ var TruckEntryForm = React.createClass({
                                 </div>
                               </div>
                             </div>
+                            <br/>
+                            
+                          <button type="submit" className="btn btn-warning">Submit</button>
                           </div>
-                          <button type="submit" className="btn btn-warning">Submit</button>                      
                         </form>
                     </div>
                 </div>
